c25c5993b4eb5ef08cbde00d81603affb727eeef
limyaoqi <limyaoqi@gmail.com>
2024-04-16 14:05:52 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const app = express();
+
+let parks = [
+  {
+    id: 1,
+    name: "Yellowstone National Park",
+    facilities: ["campgrounds", "visitor-center", "trails"],
+  },
+  {
+    id: 2,
+    name: "Zion National Park",
+    facilities: ["trails", "visitor-center"],
+  },
+];
+
+let visitors = [
+  { id: 1, name: "John Doe", pastReservations: [1], upcomingReservations: [2] },
+  { id: 2, name: "Jane Smith", pastReservations: [], upcomingReservations: [] },
+];
+
+let reservations = [
+  { id: 1, parkId: 1, visitorId: 1, date: "2023-09-01" },
+  { id: 2, parkId: 2, visitorId: 1, date: "2023-10-01" },
+];
+
+// {
+//     id: 1,
+//     name: 'John Doe',
+//     pastReservations: [
+//     {
+//          id: 1, parkId: 1, visitorId: 1, date: '2023-09-01' }
+//     ],
+//     upcomingReservations: [
+//     {
+//          id: 2, parkId: 2, visitorId: 1, date: '2023-10-01' }
+//     ],
+//     }
+
+// Your routing, authentication, and controller code goes here
+app.get("/parks", (req, res) => {
+  if (parks) {
+    res.json(parks);
+  } else {
+    res.status(404).json("parks not found");
+  }
+});
+app.get("/parks/:id", (req, res) => {
+  const selectedPark = parks.find((p) => p.id == req.params.id);
+  if (selectedPark) {
+    res.json(selectedPark);
+  } else {
+    res.status(404).json("parks not found");
+  }
+});
+app.get("/visitors", (req, res) => {
+  if (visitors) {
+    res.json(visitors);
+  } else {
+    res.status(404).json("visitors not found");
+  }
+});
+app.get("/visitors/:id", (req, res) => {
+  const selectedVisitor = visitors.find((v) => v.id == req.params.id);
+  if (selectedVisitor) {
+    const pReservations = reservations.find(
+      (r) => r.id == selectedVisitor.pastReservations
+    );
+    const uReservations = reservations.find(
+      (r) => r.id == selectedVisitor.upcomingReservations
+    );
+    const result = {
+      ...selectedVisitor,
+      pastReservations: pReservations ? [pReservations] :[] ,
+      upcomingReservations: uReservations ? [uReservations] :[] ,
+    };
+    res.json(result);
+  } else {
+    res.status(404).json("visitors not found");
+  }
+});
+app.get("/reservations", (req, res) => {
+  if (reservations) {
+    res.json(reservations);
+  } else {
+    res.status(404).json("reservations not found");
+  }
+});
+app.get("/reservations/:id", (req, res) => {
+  const selectedR = reservations.find((p) => p.id == req.params.id);
+  if (selectedR) {
+    res.json(selectedR);
+  } else {
+    res.status(404).json("reservations not found");
+  }
+});
+
+app.listen(5000, () => {
+  console.log("National Park Visitor System is running on port 5000");
+});
